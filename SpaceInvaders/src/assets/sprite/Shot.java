b0f403881b142cb31472459e8e5d6bda6c86ad5b
GiaTran203 <giatran2340@gmail.com>
2024-05-24 11:47:56 +0700

    update

--- SpaceInvaders/src/assets/sprite/Shot.java
+++ SpaceInvaders/src/assets/sprite/Shot.java
@@ -1,5 +1,21 @@
 package assets.sprite;
+import javax.swing.ImageIcon;
+public class Shot extends Sprite{
+    public Shot(){
+    }
+    public Shot(int x, int y){
+        initShot(x, y);
+    }
+    private void initShot(int x, int y){
+        var shotImg = "src/images/shot.png";
+        var ii = new ImageIcon(shotImg);
+        setImage(ii.getImage());
 
-public class Shot {
+        int H_SPACE = 6;
+        setX(x + H_SPACE);
 
+        int V_SPACE = 1;
+        setY(y - V_SPACE);
+    }
 }
+
